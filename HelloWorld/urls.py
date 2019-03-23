8fe13d331e6d015a3f305949c21e793c55457b61
xx <xuxin811@163.com>
2019-03-23 15:07:20 +0800

    asd

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from learn import views as learn_views  # new
 
 urlpatterns = [
+    path('', learn_views.index),  # new
     path('admin/', admin.site.urls),
 ]
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\workspace\Pyspace\HelloWorld\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
